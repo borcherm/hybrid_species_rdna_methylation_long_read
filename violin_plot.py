e9c4a13d134969298d4e7b7e4c2a581d280bd0e0
borcherm <51982036+borcherm@users.noreply.github.com>
2026-05-27 14:32:40 -0500

    Add files via upload
    update violin plot script.

--- violin_plot.py
+++ violin_plot.py
@@ -2,7 +2,9 @@ import sys
 import seaborn
 import pandas as pd
 
-df = pd.read_csv('read_summary_split_filtered.txt', sep='\t', names=['group', 'read', 'num', 'meth_pct'])
+file = sys.argv[1]
+
+df = pd.read_csv(file, sep='\t', names=['group', 'type', 'read', 'meth_pct'])
 df_filtered = df[df['group'] != 'none']
 
 seaborn.set(rc={'figure.figsize':(19,19)})
@@ -10,4 +12,6 @@ seaborn.set(style = 'whitegrid')
 seaborn.set(font_scale=2.0)
 plot = seaborn.violinplot(x=df_filtered['group'], y=df_filtered['meth_pct'], data=df_filtered, linewidth=0.5, inner='point', cut=0)
 plot.set_xticklabels(plot.get_xticklabels(), rotation=45)
-plot.figure.savefig("output.png")
+
+filename = file.split(".")[0]
+plot.figure.savefig(filename+".output.png")
